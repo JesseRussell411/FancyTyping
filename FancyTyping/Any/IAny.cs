bfdb4e132f091826da7c14ee0fdbcd1a2702e622
Jesse Russell <jesserussell411@gmail.com>
2021-06-06 02:03:58 -0600

    Added the Type property to the IAny interface

--- FancyTyping/Any/IAny.cs
+++ FancyTyping/Any/IAny.cs
@@ -12,6 +12,6 @@ namespace JesseRussell.FancyTyping
         IEnumerable<Type> WhiteList { get; }
         ImmutableHashSet<Type> WhiteSet { get; }
         object Value { get; }
-
+        Type Type { get; }
     }
 }
